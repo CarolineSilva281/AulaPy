037286b554e7182524ed7958555349feef6193a4
CarolineSilva281 <carolinepereira2535@gmail.com>
2025-07-03 22:22:00 -0300

    Add files via upload

---
+++ 01/exercicioaula0002.py
@@ -0,0 +1,12 @@
+#vranco verde vermelho
+print ('bahia')
+print ('paysandu')
+print (' fortaleza')
+time =int(input(' Digite um numero e veja seu time'))
+if time == 1:
+    print ('bahia')
+
+if time == 2:
+ print ('paysandu') 
+
+ else time=  
